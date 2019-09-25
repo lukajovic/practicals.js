40f4cc98baa6237055a33a63b9b5a624243b8169
Luka Jovic <lukkavp@msn.com>
2019-09-25 22:49:06 +0200

    Finished

---
+++ practical16.js
@@ -0,0 +1,13 @@
+// 1. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+// number is odd or even, and display a message to the screen.
+
+for (let index = 0; index <= 15; index++) {
+
+    if (index % 2 == 0) {
+
+        console.log(index + " - " + "This number is even");
+
+    } else console.log(index + " - " +" This number is odd");
+
+
+}
