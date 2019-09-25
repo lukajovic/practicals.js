215308da7848289ab4086d2f7cacb392ef8bb71a
Luka Jovic <lukkavp@msn.com>
2019-09-25 21:05:33 +0200

    Finished

---
+++ practical6.js
@@ -0,0 +1,34 @@
+// 1. Write a program that shows text representation of a day in a week for a number input from
+// 1 to 7. Print output in console.
+// For input 1 , output should be “ Monday”.
+
+switch (5) {
+
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+
+
+
+
+
+
+
+}
