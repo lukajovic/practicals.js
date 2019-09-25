93aca40d711d7d57a8946982103c215a0d71047e
Luka Jovic <lukkavp@msn.com>
2019-09-25 20:53:48 +0200

    Finished

---
+++ practical5.js
@@ -0,0 +1,11 @@
+// Task 5. Write a program that compares two numbers and displays the larger. Display the result in
+// the console.
+
+var a = 5;
+var b = 4;
+
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log(b)
+} else console.log("The numbers are equal");;
