bf5a928aabf224a830b840494cd90b97b2f71ffc
Luka Jovic <lukkavp@msn.com>
2019-09-25 23:58:02 +0200

    Finished

---
+++ practical23.js
@@ -0,0 +1,13 @@
+// 1. Write a program that calculates the maximum of two given numbers.
+
+function maximum(a, b) {
+
+    if (a > b) {
+        return a
+    } else if (a < b) {
+        return b
+    } else return "The numbers are equal"
+
+}
+
+console.log(maximum(5,5));
