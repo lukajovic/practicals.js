98848eb0d47babf7c6ac8bae6e1ade08cd1fe062
Luka Jovic <lukkavp@msn.com>
2019-09-25 22:17:38 +0200

    Finished

---
+++ practical13.js
@@ -0,0 +1,7 @@
+// 2. Write an arrays of days in a week. Using console.log display Sunday from the array.
+
+var array;
+
+array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+console.log(array[6]);
