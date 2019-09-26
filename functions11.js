6149f477a20dfdbc66e39fc40f59f7c9b9abe475
Luka Jovic <lukkavp@msn.com>
2019-09-26 16:51:12 +0200

    Finished

---
+++ functions11.js
@@ -0,0 +1,19 @@
+// 2. Write a function to check whether a string is blank or not.
+// "My random string" -> false
+// " " -> true
+// 12 -> false
+// false -> false;
+
+function stringIsBlank(x) {
+
+
+    if (x === " ") {
+
+        return "true";
+
+    } else return "false";
+
+
+}
+
+console.log(stringIsBlank(" "));
