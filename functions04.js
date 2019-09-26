dc7d471fab399122c16a47321a83a98c42dde911
Luka Jovic <lukkavp@msn.com>
2019-09-26 15:49:40 +0200

    Finished

---
+++ functions04.js
@@ -0,0 +1,11 @@
+// 4. Write a program that calculates an arithmetic mean of four numbers.
+
+function arithmeticMean(x, y, z, c) {
+
+    var result = (x + y + z + c) / 4;
+
+    return result;
+
+}       
+
+console.log(arithmeticMean(4,2,3,4));
